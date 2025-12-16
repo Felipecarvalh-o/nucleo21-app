b7178db11d40591f309f19a4ab1ec0a1c423e80e
Felipecarvalh-o <negocioscarvalho@outlook.com>
2025-12-15 23:25:43 -0300

    simulador.py

--- simulador.py
+++ simulador.py
@@ -1,30 +1,29 @@
 import random
 
-def sortear():
-    """Gera um sorteio aleatório com 6 dezenas"""
-    return sorted(random.sample(range(1, 61), 6))
 
-
-def pontuar(jogo, sorteio):
-    """Conta quantos acertos"""
-    return len(set(jogo) & set(sorteio))
-
-
-def simular_cenario(jogos, simulacoes=500):
-    """
-    Simula vários sorteios aleatórios e avalia os jogos
-    Retorna estatísticas educacionais
-    """
+def simular_cenario(jogos, total_sorteios=500):
     resultados = []
+    zeros = 0
+    maximo = 0
 
-    for _ in range(simulacoes):
-        sorteio = sortear()
-        melhor_ponto = max(pontuar(jogo, sorteio) for jogo in jogos)
-        resultados.append(melhor_ponto)
+    for _ in range(total_sorteios):
+        sorteio = set(random.sample(range(1, 61), 6))
+        melhor = 0
+
+        for jogo in jogos:
+            pontos = len(sorteio & set(jogo))
+            melhor = max(melhor, pontos)
+
+        resultados.append(melhor)
+        if melhor == 0:
+            zeros += 1
+        maximo = max(maximo, melhor)
+
+    media = round(sum(resultados) / len(resultados), 2)
 
     return {
-        "media": round(sum(resultados) / len(resultados), 2),
-        "maximo": max(resultados),
-        "zeros": resultados.count(0),
-        "acima_4": len([r for r in resultados if r >= 4])
+        "media": media,
+        "maximo": maximo,
+        "zeros": zeros,
+        "total": total_sorteios
     }
